6273ee2f7e28dacb8cb7a1d48ffb049ff4c752a2
Pusan Chakraborty <pusanc2@illinois.edu>
2025-03-31 22:43:52 -0500

    Add files via upload

--- lab4_func.py
+++ lab4_func.py
@@ -3,25 +3,6 @@ import numpy as np
 from scipy.linalg import expm
 from lab4_header import *
 
-# Test 1 (100, 100, 150, 90):
-# x = 99 mm
-# y = 103 mm
-# z = 145 mm
-#
-# Test 2 (100, 500, 100, 90):
-# x = 98 mm
-# y = 504 mm
-# z = 96 mm
-
-# helper functions for lab_fk
-def VecTose3(V):
-	return np.r_[np.c_[VecToso3([V[0], V[1], V[2]]), [V[3], V[4], V[5]]], np.zeros((1,4))]
-
-def VecToso3(omg):
-	return np.array([[0, -omg[2], omg[1]],[omg[2], 0, -omg[0]], [-omg[1], omg[0], 0]])
-
-
-
 """
 Use 'expm' for matrix exponential.
 Angles are in radian, distance are in meters.
@@ -29,37 +10,36 @@ Angles are in radian, distance are in meters.
 def Get_MS():
 	# =================== Your code starts here ====================#
 	# Fill in the correct values for a1~6 and q1~6, as well as the M matrix
- 
-	M = np.array([[0, -1, 0, -150 + 244 + 213 + 83],
-               	  [0, 0, -1, 150 + 120 - 93 + 83 + 82 + 59],
-                  [1, 0,  0, 215.5],
-                  [0, 0,  0, 1],])
-	
- 
-	q1 = np.array([-150, 				  150, 				   10])
-	q2 = np.array([-150, 				  150 + 120, 		   10 + 152])
-	q3 = np.array([-150 + 244,			  150 + 120, 		   10 + 152])
-	q4 = np.array([-150 + 244 + 213,	  150 + 120 - 93, 	   10 + 152])
-	q5 = np.array([-150 + 244 + 213,      150 + 120 - 93 + 83, 10 + 152])
-	q6 = np.array([-150 + 244 + 213 + 83, 150 + 120 - 93 + 83, 10 + 152])
+	M = np.eye(4)
+	S = np.zeros((6,6))
+	M = np.array([
+        [0, -1, 0, 0.39],
+        [0, 0, -1, 0.401],
+        [1, 0, 0, 0.2155],
+        [0, 0, 0, 1]
+     ])
+	w_list = np.array([
+	[0, 0, 1],
+	[0, 1, 0],
+	[0, 1, 0],
+	[0, 1, 0],
+	[1, 0, 0],
+	[0, 1, 0]
+	])
 
-	w1 = np.array([0,0,1])
-	w2 = np.array([0,1,0])
-	w3 = np.array([0,1,0])
-	w4 = np.array([0,1,0])
-	w5 = np.array([1,0,0])
-	w6 = np.array([0,1,0])
-	
-	S1 = np.block([w1, np.cross(-w1,q1)])
-	S2 = np.block([w2, np.cross(-w2,q2)])
-	S3 = np.block([w3, np.cross(-w3,q3)])
-	S4 = np.block([w4, np.cross(-w4,q4)])
-	S5 = np.block([w5, np.cross(-w5,q5)])
-	S6 = np.block([w6, np.cross(-w6,q6)])
- 
-	S = np.block([[S1], [S2], [S3], [S4], [S5], [S6]])
- 
+	q_list = np.array([
+	[-0.15, 0.15, 0],
+	[-0.15, 0, 0.162],
+	[0.094, 0, 0.162],
+	[0.307, 0, 0.162],
+	[0, 0.260, 0.162],
+	[0.390, 0, 0.162]
+	])
 
+	S = np.column_stack([np.concatenate((w, np.cross(-w, q))) for w, q in zip(w_list, q_list)])
+
+	print(M, "\n")
+	print(S, "\n")
 
 	# ==============================================================#
 	return M, S
@@ -73,30 +53,44 @@ def lab_fk(theta1, theta2, theta3, theta4, theta5, theta6):
 	# Initialize the return_value
 	return_value = [None, None, None, None, None, None]
 
-	print("Thetas calculated:\n")
- 
+	print("Foward kinematics calculated:\n")
 
 	# =================== Your code starts here ====================#
-	theta = np.degrees(np.array([theta1,theta2,theta3,theta4,theta5,theta6]))
- 
-	print(str(theta) + '\n')
- 
+	theta = np.array([theta1,theta2,theta3,theta4,theta5,theta6])
 	T = np.eye(4)
+
 	M, S = Get_MS()
+
+
+	T = np.eye(4)
  
-	S1_mat = VecTose3(S[0])
-	S2_mat = VecTose3(S[1])
-	S3_mat = VecTose3(S[2])
-	S4_mat = VecTose3(S[3])
-	S5_mat = VecTose3(S[4])
-	S6_mat = VecTose3(S[5])
- 
-	T = expm(S1_mat * theta1) @ expm(S2_mat * theta2) @ expm(S3_mat * theta3) @ expm(S4_mat * theta4) @ expm(S5_mat * theta5) @ expm(S6_mat * theta6) @ M
+	for i in range(6):
+
+		# Extract w,v
+		w = S[:3, i]
+		v = S[3:, i]
 	
-	print("Foward kinematics calculated:\n")
+		# Build S matrix
+		S_matrix = np.zeros((4, 4))
+		S_matrix[0:3, 0:3] = np.array([
+			[0, -w[2], w[1]],
+			[w[2], 0, -w[0]],
+			[-w[1], w[0], 0]
+		])
+		S_matrix[0:3, 3] = v
+	
+		# Matrix exponential
+		T_i = expm(S_matrix * theta[i])
+	
+		T = T @ T_i
+	
+	T = T @ M
+
+	#np.set_printoptions(suppress=True, precision=8)
+
+	# ==============================================================#
+
 	print(str(T) + "\n")
-
-
 	# ==============================================================#
 
 	return_value[0] = theta1 + PI
@@ -114,77 +108,67 @@ Function that calculates an elbow up Inverse Kinematic solution for the UR3
 """
 def lab_invk(xWgrip, yWgrip, zWgrip, yaw_WgripDegree):
 	# =================== Your code starts here ====================#
+
+	L1 = 0.152
+	L2 = 0.120
+	L3 = 0.244
+	L4 = 0.093
+	L5 = 0.213
+	L6 = 0.083
+	L7 = 0.083
+	L8 = 0.082
+	L9 = 0.0535
+	L10 = 0.059
+	al_plate = 0.027
+
+	#deg to rad convert
+	yaw = np.deg2rad(yaw_WgripDegree)
+ 
+	# coordinate conversion
+	xgrip = xWgrip + 0.15
+	ygrip = yWgrip - 0.15
+	zgrip = zWgrip - 0.01
 	
-	# convert World coordinates to Base coordinates (frame 0)
-	x_grip = xWgrip + 150
-	y_grip = yWgrip - 150
-	z_grip = zWgrip - 10
-	yaw_WgripRadians = np.radians(yaw_WgripDegree)
-	
-	# arm lengths
-	L1 = 152
-	L2 = 120
-	L3 = 244
-	L4 = 93
-	L5 = 213
-	L6 = 83
-	L7 = 83
-	L8 = 82
-	L9 = 53.5
-	L10 = 59
+	xcenter = xgrip - L9 * np.cos(yaw)
+	ycenter = ygrip - L9 * np.sin(yaw)
+	zcenter = zgrip
+	 
+	# theta 5
+	theta5 = -np.pi / 2
  
-	# find wrist's center point
-	x_cen = x_grip - L9*np.cos(yaw_WgripRadians)
-	y_cen =	y_grip - L9*np.sin(yaw_WgripRadians)
-	z_cen = z_grip
-	
-	# find theta1 (waist angle)
-	if (((L2-L4+L6)/np.sqrt(x_cen**2 + y_cen**2)) > 1):
-		print("WARNING: INVALID POSITION")
+ 	# theta1
+	hyp = (np.sqrt(xcenter**2 + ycenter**2))
+	theta_small = np.arcsin((L6 + al_plate) / hyp)
+	theta_large = np.arctan2(ycenter, xcenter)
+	theta1 = theta_large - theta_small
  
-	theta_small = np.arcsin((L2-L4+L6)/np.sqrt(x_cen**2 + y_cen**2))
-	theta_big = np.arctan2(y_cen, x_cen)
-	theta1 = theta_big - theta_small
+	#theta 6 
+	theta6 = theta1 + np.pi / 2 - yaw 	
  
-	
-	# print("x_cen, y_cen, z_cen:", x_cen, y_cen, z_cen)
-	# print("theta_small:", np.degrees(theta_small), "theta_big:", np.degrees(theta_big))
-	
+	#projected end point
+	z3end = zcenter + L10 + L8
+	x3end = ((hyp * np.cos(theta_small)- L7) * np.cos(theta1)) 
+	y3end = ((hyp * np.sin(theta_small)- L7) * np.sin(theta1))
  
-	# find theta6
-	theta6 = np.pi/2 + theta1 - yaw_WgripRadians
- 
-	# find projected endpoint
-	x_3end = x_cen - L7*np.cos(theta1) + (L6+27)*np.sin(theta1)
-	y_3end = y_cen - L7*np.sin(theta1) - (L6+27)*np.cos(theta1)
-	z_3end = z_cen + L10 + L8 
- 
-	# find theta2, theta3, theta4
-	hypotenuse_xy = np.sqrt((x_3end)**2 + (y_3end)**2)	
-	
-	d = np.sqrt((z_3end - L1)**2 + (hypotenuse_xy)**2)
-	thetau = np.arccos((L3**2 + L5**2 - d**2)/(2*L3*L5))
-	
-	theta3 = np.pi - thetau
- 
-	theta2_big = np.arccos((L3**2 + d**2 - L5**2)/(2*L3*d))
-	theta2_small = np.arctan2((z_3end - L1), (hypotenuse_xy))
-	theta2 = -(theta2_big + theta2_small)
- 
-	theta4 = -(theta3 + theta2)
- 
- 
-	theta5 = -np.pi/2
+	#theta3
+	zoffset = z3end - L1
+	r = np.sqrt(x3end**2 + y3end**2)
+	h = np.sqrt(r**2 + zoffset**2)
+	theta3 = np.pi - np.arccos((L5**2 + L3**2 - h**2)/(2 * L5 * L3))
+
+	#theta2
+	theta2a = np.arccos((L3**2 + h**2 - L5**2) / (2 * L3 * h))
+	theta2b = np.arctan2(zoffset,r)
+	theta2 = -(theta2a + theta2b)
+
+	#theta4
+	theta4a = np.pi - (np.pi - theta3) + theta2a
+	smallangle = np.pi/2 - theta4a 
+	theta4 = -(np.pi - np.pi/2 - theta2b - smallangle)
+  
+	print("theta1: ", theta1 * 180 / np.pi, "\ntheta2: ", theta2 * 180 / np.pi, 
+       "\ntheta3: ", theta3 * 180 / np.pi, "\ntheta4: ", theta4 * 180 / np.pi, 
+       "\ntheta5: ", theta5 * 180 / np.pi, "\ntheta6: ", theta6 * 180 / np.pi)
+
 	# ==============================================================#
-	return lab_fk(theta1, 
-               	theta2, 
-                theta3, 
-                theta4, 
-                theta5, 
-                theta6)
-	# return lab_fk(np.degrees(theta1), 
-    #            	np.degrees(theta2), 
-    #             np.degrees(theta3), 
-    #             np.degrees(theta4), 
-    #             np.degrees(theta5), 
-    #             np.degrees(theta6))
+	return lab_fk(theta1, theta2, theta3, theta4, theta5, theta6)
